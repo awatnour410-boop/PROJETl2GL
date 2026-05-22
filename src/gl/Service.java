46015c964bf332a245309f1b29c233b59a8af927
noura <noura@NOUR>
2026-05-22 16:54:44 +0100

    comite 5 service.java LAOUSSON YAYA

---
+++ src/gl/Service.java
@@ -0,0 +1,19 @@
+package gl;
+
+public class Service {
+    private String nom;
+    private String responsable;
+    private String agents;
+    private String horaires;
+    private String prestations;
+    private String explicationFonction;
+
+    public Service(String nom, String responsable, String agents, String horaires, String prestations, String explicationFonction) {
+        this.nom = nom;
+        this.responsable = responsable;
+        this.agents = agents;
+        this.horaires = horaires;
+        this.prestations = prestations;
+        this.explicationFonction = explicationFonction;
+    }
+
